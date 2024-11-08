93c8e777f123822d26bf079599e87072690ada31
Sam <ssteven352@gmail.com>
2024-11-08 11:39:58 +0000

    change color after documnet load

--- public/js/init.js
+++ public/js/init.js
@@ -1,8 +1,17 @@
 const HealthyColour = "#05ff16";
 const StoppedColour = "#ff0505";
 const changeColour = (id, colourCode) => {
-	var element = document.getElementById(id);
-	element.setAttribute("style", `fill: ${colourCode};`);
+	var svgObject = document.getElementById("VDL_ARRIVALS_layout").contentDocument;
+	if (svgObject) {
+        var element = svgObject.getElementById(id);
+        if (element) {
+            element.setAttribute("style", `fill: ${colourCode};`);
+        } else {
+            console.error(`Element with id "${id}" not found in the SVG.`);
+        }
+    } else {
+        console.error("SVG document not loaded.");
+    }
 };
 
 var a = document.getElementById("VDL_ARRIVALS_layout");
